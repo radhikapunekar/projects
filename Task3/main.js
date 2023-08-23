44cbf89d5a425da446f9cd4fb1f781c926eabbb7
radhikapunekar <fulzeleradhika13@gmail.com>
2023-08-23 15:26:50 +0530

    filtering added

--- Task3/main.js
+++ Task3/main.js
@@ -1,11 +1,14 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filter = document.getElementById('filter');
 
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+//filter event
+filter.addEventListener('keyup',filterItems);
 
 // Add item
 function addItem(e){
@@ -13,13 +16,16 @@ function addItem(e){
 
   // Get input value
   let newItem = document.getElementById('item').value;
+  let newDescipt=document.getElementById('description').value;
 
   // Create new li element
   let li = document.createElement('li');
+  
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newDescipt));
 
   // Create del button element
   let deleteBtn = document.createElement('button');
@@ -55,4 +61,20 @@ function removeItem(e){
     }
   } 
 }
-
+//Filter item
+function filterItems(e){
+    // convert text to lowercase
+    let text = e.target.value.toLowerCase();
+    // Get list
+    let items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+    let itemName = item.firstChild.textContent;
+    let itemdes = item.childNodes[1].textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1 || itemdes.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
